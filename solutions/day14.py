5f493cc611ff459aa7ce7896775fb012bd25dfa8
ryan-heslin <rwheslin@gmail.com>
2024-12-31 13:42:42 -0500

    Improve day 14

--- solutions/day14.py
+++ solutions/day14.py
@@ -4,20 +4,7 @@ from itertools import product
 from operator import mul
 from utils.utils import split_lines, count_dict, list_map
 
-
-def parse(line):
-    nums = re.findall(r"-?\d+", line)
-    return dict(zip(("xpos", "ypos", "xdel", "ydel"), map(int, nums)))
-
-
-def display(graph, xmax, ymax):
-    return "\n".join(
-        "".join("#" if graph[complex(col, row)] else " " for col in range(xmax))
-        for row in range(ymax)
-    )
-
-# Sigh....
-pattern  = """###############################
+pattern = """###############################
 #                             #
 #                             #
 #                             #
@@ -50,20 +37,43 @@ pattern  = """###############################
 #                             #
 #                             #
 ###############################"""
+TOP = STUMP = None
+
+for line in pattern.splitlines():
+    if re.match(r"^#\s{4}#+\s{4}#$", line):
+        TOP = re.compile("^.*(" + line + ").*$")
+    elif re.match(r"^#\s+#{3}\s+#$", line):
+        STUMP = line
+
+assert TOP is not None and STUMP is not None
+
+def parse(line):
+    nums = re.findall(r"-?\d+", line)
+    return dict(zip(("xpos", "ypos", "xdel", "ydel"), map(int, nums)))
+
+
+def display(graph, xmax, ymax):
+    return "\n".join(
+        "".join("#" if graph[complex(col, row)] else " " for col in range(xmax))
+        for row in range(ymax)
+    )
+
+
+# Sigh....
+
 
 def is_ascending(l):
     # https://stackoverflow.com/questions/34684461/check-if-a-list-contains-incrementing-elements-starting-from-zero
     return l == list(range(l[0], len(l)))
 
+
 def find_tree(data, xmax, ymax):
-    graph = {complex(x, y) : 0 for x, y in product(range(xmax), range(ymax))}
+    graph = {complex(x, y): 0 for x, y in product(range(xmax), range(ymax))}
     for pair in data:
         graph[complex(pair["xpos"], pair["ypos"])] += 1
 
-    iteration = 0 
-    output = "day14.txt"
+    iteration = 0
     # TODO: check for horizontal and vertical bars of the correct length?
-    greatest = []
     while iteration < 11000:
         for i, pair in enumerate(data):
             xpos = pair["xpos"]
@@ -78,15 +88,20 @@ def find_tree(data, xmax, ymax):
             graph[complex(xpos, ypos)] += 1
 
         iteration += 1
-        #print(iteration)
-        text = display(graph, xmax, ymax) 
-        greatest.append(max(map(len, re.findall("#{2,}", text))))
-        # with open(output, "a") as f:
-        #     f.write(str(iteration))
-        #     f.write(text)
-        #     f.write("\n\n\n")
-        #print("\n\n\n")
-    return greatest
+        # if iteration == 7603:
+        #     breakpoint()
+        text = display(graph, xmax, ymax).splitlines()
+        for i, line in enumerate(text):
+            m = re.match(TOP, line)
+            if m:
+                span = m.span(1)
+                if (i < len(text) - 3) and (
+                    text[i + 1][span[0] : span[1]]
+                    == text[i + 2][span[0] : span[1]]
+                    == text[i + 3][span[0]: span[1]]
+                    == STUMP
+            ):
+                    return iteration
 
 
 def count(data, xlim, ylim, n):
@@ -99,14 +114,8 @@ def count(data, xlim, ylim, n):
     ymid = ylim // 2
 
     for pair in data:
-        # if pair == {'xpos': 2, 'ypos': 4, 'xdel': 2, 'ydel': -3}:
-        #     breakpoint()
         xpos = pair["xpos"]
         ypos = pair["ypos"]
-        # xpos = xpos + pair["xdel"] * n
-        # ypos = ypos + pair["ydel"] * n
-        # xpos %= n
-        # ypos %= n
         for _ in range(n):
             xpos += pair["xdel"]
             ypos += pair["ydel"]
@@ -126,7 +135,6 @@ def count(data, xlim, ylim, n):
         positions[complex(xpos, ypos)] += 1
 
     quadrants.pop(-1)
-    print(quadrants)
     return reduce(mul, quadrants.values())
 
 
@@ -136,4 +144,5 @@ xmax = 101
 ymax = 103
 part1 = count(parsed, xmax, ymax, 100)
 print(part1)
-greatest = find_tree(parsed, xmax, ymax)
+part2 = find_tree(parsed, xmax, ymax)
+print(part2)
