c952cb6435096b2851c782c2ab934bcc18375e33
ryan-heslin <rwheslin@gmail.com>
2025-02-11 14:34:22 -0500

    Improve day 18

--- solutions/day18.py
+++ solutions/day18.py
@@ -2,21 +2,14 @@ from utils.utils import split_lines, manhattan
 from math import inf
 from collections import defaultdict
 import heapq
-# alt04 — Today at 2:31 PM
-# TODO
-# I thought about it and what the algorithm will basically do is:
-# put all blocks down. apply djikstra (flood fill).
-#
-# Remove a block, knowing where the block was removed: continue djikstra/flood fill from there. Repeat untill you removed enough blocks to reach the target.
 
 class Coord(complex):
-    def __new__(cls, real, imag=0.0, target=0.0):
+    def __new__(cls, real, imag=0.0, target: float|complex=0.0):
         this = super().__new__(cls, real, imag)
-        # Set the additional attribute
         this.target = target
         return this
 
-    def __init__(self, real, imag=0.0, target=0.0):
+    def __init__(self, real, imag=0.0, target :float | complex=0.0):
         self.target = target
 
     def __add__(self, value: complex, /) -> complex:
@@ -35,15 +28,16 @@ class Coord(complex):
 def parse(lines, n=None):
     if n is None:
         n = len(lines)
-    return (complex(*map(int, line.split(","))) for line in lines[:n])
+    return [ complex(*map(int, line.split(","))) for line in lines[:n] ]
 
 
-def A_star(graph, start, end, xmax, ymax, verify = False):
+def A_star(graph, start, end, xmax, ymax, coords):
     start = Coord(start, target=end)
     end = Coord(end, target=end)
     open_set = [start]
     heapq.heapify(open_set)
     directions = {-1j, 1, 1j, -1}
+    coord_index = len(coords) - 1
 
     g_score = defaultdict(lambda: inf)
     g_score[start] = 0
@@ -51,47 +45,56 @@ def A_star(graph, start, end, xmax, ymax, verify = False):
     cutoff = inf
     while True:
         try:
-            current = heapq.heappop(open_set)
+            index = heapq.heappop(open_set)
         except IndexError:
-            break
-        if current == end:
-            if verify:
-                return g_score[current]
-            cutoff = min(cutoff, g_score[current])
+            if cutoff < inf:
+                break
+            cost =  inf
+            new = Coord(0, 0)
+            while cost == inf:
+                new = Coord(coords[coord_index], target = complex(end.real, end.imag))
+                graph.remove(new)
+                coord_index -= 1
+                cost = min(g_score[dir + new] for dir in directions)
+            #breakpoint()
+            g_score[new] = cost + 1
+            heapq.heappush(open_set, new)
             continue
-        if g_score[current] >= cutoff:
+        if index == end:
+            cutoff = min(cutoff, g_score[index])
+            continue
+        if g_score[index] >= cutoff:
             continue
 
         for dir in directions:
-            new_coord = current + dir
+            new_coord = index + dir
             if new_coord in graph or not (
                 0 <= new_coord.real <= xmax and 0 <= new_coord.imag <= ymax
             ):
                 continue
-            new_g_score = g_score[current] + 1
+            new_g_score = g_score[index] + 1
             if new_g_score < g_score[new_coord]:
                 g_score[new_coord] = new_g_score
                 heapq.heappush(open_set, new_coord)
 
-    return cutoff
-
-
-def find_block(graph, start, end, xmax, ymax, coords):
-    for coord in coords:
-        graph.add(coord)
-        result = A_star(graph, start, end, xmax, ymax, verify=True)
-        if result == inf:
-            return f"{int(coord.real)},{int(coord.imag)}"
+    if coord_index < len(coords) -1:
+        coord = coords[coord_index + 1]
+        return f"{int(coord.real)},{int(coord.imag)}"
+    else:
+        return cutoff
 
 
 lines = split_lines("inputs/day18.txt")
 n = 1024
-graph = set(parse(lines, n))
+first = parse(lines, n)
 xmax = ymax = 70
 start = 0
 end = complex(xmax, ymax)
-part1 = A_star(graph, 0, end, xmax, ymax)
+part1 = A_star(set(first), 0, end, xmax, ymax, first)
 print(part1)
 
-part2 = find_block(graph, start, end, xmax, ymax, list(parse(lines[n:])))
+second = parse(lines[n:])
+coords = first + second
+
+part2 = A_star(set(coords), start, end, xmax, ymax, coords)
 print(part2)
